002607ff8f1e5ba86e9baa74e56ed3e879b09f42
zaitt <90521247+zaitt4@users.noreply.github.com>
2022-12-01 15:00:46 +0200

    Add files via upload

---
+++ lb6/lb6s3/lb6s3/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lb6s3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.YellowGreen;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(12, 38);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(776, 400);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.simpleOpenGlControl1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "(*.png)|*.png";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ lb6/lb6s3/lb6s3/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.DevIl;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace lb6s3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+        }
+        // Ряд допоміжних змінних - поворот
+        private int rot = 0;
+        // прапор – завантажена чи текстура
+        private bool textureIsLoad = false;
+        // ім'я текстури
+        public string texture_name = "";
+        // Ідентифікатор текстури
+        public int imageId = 0;
+        // текстурний об'єкт
+        public uint mGlTextureObject = 0;
+        double x = 0, y = 0;
+
+
+    private void Form1_Load(object sender, EventArgs e)
+        {
+        // ініціалізація бібліотеки glut
+        Glut.glutInit();
+        // ініціалізація режиму екрану
+        Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+        // ініціалізація бібліотеки openIL
+        Il.ilInit();
+
+        Il.ilEnable(Il.IL_ORIGIN_SET);
+        // встановлення кольору очищення екрану (RGBA)
+        Gl.glClearColor(255, 255, 255, 1);
+        // Установка порту висновку
+        Gl.glViewport(0, 0, simpleOpenGlControl1.Width, simpleOpenGlControl1.Height);
+        // активація проекційної матриці
+        Gl.glMatrixMode(Gl.GL_PROJECTION);
+        // Очищення матриці
+        Gl.glLoadIdentity();
+        // Встановлення перспективи
+        Glu.gluPerspective(30, simpleOpenGlControl1.Width / simpleOpenGlControl1.Height, 1, 100);
+        // Установка об'єктно - видовий матриці
+        Gl.glMatrixMode(Gl.GL_MODELVIEW); Gl.glLoadIdentity();
+        // Початкові налаштування OpenGL
+        Gl.glEnable(Gl.GL_DEPTH_TEST);
+        Gl.glEnable(Gl.GL_LIGHTING);
+        Gl.glEnable(Gl.GL_LIGHT0);
+        timer1.Start(); // Активація таймера
+        
+    }
+
+    private void openToolStripMenuItem_Click(object sender, EventArgs e)
+    {            
+        // відкриваємо вікно вибору файлу
+        DialogResult res = openFileDialog1.ShowDialog();
+        // якщо файл вибраний - і повернуто результат OK
+        if (res == DialogResult.OK)
+            {
+                // створюємо зображення з ідентифікатором imageId
+                Il.ilGenImages(1, out imageId);
+                // робимо зображення поточним
+                Il.ilBindImage(imageId);
+                // адреса зображення отримана за допомогою вікна вибору файлу
+                string url = openFileDialog1.FileName;
+                // пробуємо завантажити зображення
+                if (Il.ilLoadImage(url))
+                {
+                    // якщо завантаження пройшло успішно
+                    // Зберігаємо розміри зображення
+                    int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                    int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+                    // Визначаємо число біт на піксель
+                    int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+                    switch (bitspp)
+                    // Залежно від отриманого результату
+                    { // створюємо текстуру, використовуючи режим GL_RGB чи GL_RGBA
+                        case 24:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                            break;
+                        case 32:
+                            mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                            break;
+                    } // активуємо прапор, що сигналізує завантаження текстури
+                    textureIsLoad = true;
+                    // Очищаємо пам'ять
+                    Il.ilDeleteImages(1, ref imageId);
+                }
+            }
+        }
+
+        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        { // Ідентифікатор текстурного об'єкта
+            uint texObject; // генеруємо текстурний об'єкт
+            Gl.glGenTextures(1, out texObject);
+            // встановлюємо режим пакування пікселів
+            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+            // створюємо прив'язку до щойно створеної текстури
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+            // встановлюємо режим фільтрації та повторення текстури
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+            // створюємо RGB чи RGBA текстуру
+            switch (Format)
+            {
+                case Gl.GL_RGB:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+                case Gl.GL_RGBA:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+            }
+            // Повертаємо ідентифікатор текстурного об'єкта
+            return texObject;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // виклик функції відтворення сцени
+            Draw();
+        }
+        // функція малювання
+        private void Draw()
+        {
+            if (textureIsLoad)
+            {
+                rot = 180; // збільшуємо кут повороту
+                //if (rot > 360) // Коригуємо кут
+                    //rot = 0;
+                // очищення буфера кольору та буфера глибини
+                Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT); Gl.glClearColor(255, 255, 255,
+               1);
+                Gl.glLoadIdentity(); // очищення поточною матриці
+                Gl.glEnable(Gl.GL_TEXTURE_2D); // Включаємо режим текстурування
+                                               // Включаємо режим текстурування, вказуючи ідентифікатор mGlTextureObject
+                Gl.glBindTexture(Gl.GL_TEXTURE_2D, mGlTextureObject);
+                Gl.glPushMatrix(); // Зберігаємо стан матриці
+                Gl.glTranslated(x + 1, y - 1.5, -5); // виконуємо переміщення для наочнішої вистави сцени
+                Gl.glRotated(rot, 0, 1, 0); // реалізуємо поворот об'єкта
+                Gl.glBegin(Gl.GL_QUADS); // Малюємо полігон
+                                         // вказуємо по черзі вершини та текстурні координати
+                Gl.glVertex3d(1, 1, 0);
+                Gl.glTexCoord2f(0, 0);
+                Gl.glVertex3d(1, 0, 0);
+                Gl.glTexCoord2f(1, 0);
+                Gl.glVertex3d(0, 0, 0);
+                Gl.glTexCoord2f(1, 1);
+                Gl.glVertex3d(0, 1, 0);
+                Gl.glTexCoord2f(0, 1);
+                // Завершуємо малювання
+                Gl.glEnd();
+                // Повертаємо матрицю
+                Gl.glPopMatrix();
+                // відключаємо режим текстурування
+                Gl.glDisable(Gl.GL_TEXTURE_2D);
+                // оновлюємо елемент зі сценою
+                simpleOpenGlControl1.Invalidate();
+            }
+        }
+
+        private void simpleOpenGlControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.Location.X / 215.0 - 1.8;
+
+            if (x < -1.5) { x = -1.5; }          
+            if (x > 0.5) { x = 0.5; }
+           
+
+            y = (500 - e.Location.Y) / 120.0 - 1.8;
+
+            if (y > 1.87) { y = 1.87; }
+            if (y < 0.14) { y = 0.14; }
+           
+        }
+    }
+
+}
+
